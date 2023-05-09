45ebec21254bd9128a567ddbf7349f7a8b11ebf8
Magomed Saybulaev <m.saybulaev@x-partners.com>
2023-05-09 19:43:16 +0300

    connect mongodb localhost; add diagram

--- server/index.js
+++ server/index.js
@@ -1,1 +1,23 @@
-console.log('test start');
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+
+const app = express()
+const PORT = config.get('serverPort')
+
+
+const start = async () => {
+    try {
+
+        await mongoose.connect(config.get('db'));
+
+
+        app.listen(PORT, () => {
+            console.log('Server start', PORT);
+        })
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();

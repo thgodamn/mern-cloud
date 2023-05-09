7a4f271a521416fcf7a3453ded1a9ed23c56e9ab
Magomed Saybulaev <m.saybulaev@x-partners.com>
2023-05-10 00:28:00 +0300

    add routes; add route - registration

--- server/index.js
+++ server/index.js
@@ -1,22 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const config = require('config');
-
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+const authRouter = require('./routes/auth.routes')
 const app = express()
 const PORT = config.get('serverPort')
+
+app.use(express.json())
+app.use('/api/auth', authRouter)
 
 
 const start = async () => {
     try {
 
-        await mongoose.connect(config.get('db'));
+        await mongoose.connect(config.get('db'))
 
 
         app.listen(PORT, () => {
-            console.log('Server start', PORT);
+            console.log('Server start', PORT)
         })
     } catch (e) {
-        console.log(e);
+        console.log(e)
+        res.send({message: `Server error ${e}`})
     }
 }
 
